f827eb92a4844ba5e92a457980d6315879d43721
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-07-09 21:45:44 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='StreamDiffer',
+    version='0.0.1',
+    description='Compare and clusterize multiple data streams.',
+    author='Roberto Abdelkader Martínez Pérez',
+    author_email='robertomartinezp@gmail.com',
+    url='https://github.com/nilp0inter/streamdiffer',
+    packages=['streamdiffer'])
